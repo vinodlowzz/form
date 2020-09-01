87558806293fc34b448d39523d0417ca5f04c45f
vinodlowzz <vinodvld10@gmail.com>
2020-09-01 19:58:33 +0530

    edited

--- form.php
+++ form.php
@@ -1,21 +1,20 @@
 <?php
-       if(isset($_POST['submit'])){
-           $name=$_POST['name'];
-           $email=$_POST['email'];
-           $phone=$_POST['phone'];
-           $msg=$_POST['msg'];
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['msg'];
 
-           $to='lowzz260299@gmail.com';
-           $subject='Form submission';
-           $message="Name: ".$name."\n"."phone: ".$phone."\n"."message: "."\n".$msg;
-           $headers="From: ".$email;
+		$to='lowzz260299@mail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
 
-           if(mail($to,$subject,$message,$headers)){
-               echo "<h1>sent successfully"." ".$name."</h1>";
-           }
-           else{
-               echo "something wrong"
-           }
-       }
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
 ?>
-    
